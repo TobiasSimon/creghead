e6fd8eddafa2ca3eceef19ce1d2749f9704aba14
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-05-26 18:35:06 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,13 @@
+
+REGACCESS
+'''''''''
+
+saves a lot of time when defining register access macros in C.
+registers and bit(field)s are defined in a python input file using
+simple lists and tuples.
+
+features:
+   bit access: set, clear, get
+   field access: set, get
+   reserved/unused fields
+
---
+++ regs.py
@@ -0,0 +1,31 @@
+
+
+prefix = 'UM6'
+regs = [
+   ('COMM', 0x00, 32, 'register docstring',
+      [
+         ('BC_RATE',   8, 'packet broadcast frequency; freq = (280 / 255) * broadcast_rate + 20'),
+         ('BAUD_RATE', 3, 'serial port baudrate; 000: 9600, 001: 14400, 010: 19200, 011 -> 38400, 100 -> 57600, 101 -> 115200'),
+         ('GPS_BAUD',  3, 'GPS baudrate see BAUD_RATE for details'),
+         None,
+         ('SAT',  'detailed satellite status transmission'),
+         ('SUM',  'satellite summary transmission'),
+         ('VEL',  'GPS course and velocity transmission'),
+         ('REL',  'GPS relative position transmission'),
+         ('POS',  'GPS position transmission'),
+         ('TEMP', 'temperature transmission'),
+         ('COV',  'covariance matrix transmission'),
+         ('EU',   'euler angles transmission'),
+         ('QT',   'quaternion transmission'),
+         ('MP',   'processed magnetometer transmission'),
+         ('AP',   'processed accelerometer transmission'),
+         ('GP',   'processed gyro transmission'),
+         ('MR',   'raw magnetometer transmission'),
+         ('AR',   'raw accelerometer transmission'),
+         ('GR',   'raw gyro transmission'),
+         ('BEN',  'broadcast mode'),
+         None
+      ]
+   ),
+]
+
